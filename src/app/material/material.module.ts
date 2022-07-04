245b7a78f181c1e2899b919412d14c8f5a1b6ac0
Emanuel <hugofer000@gmail.com>
2022-07-04 14:53:13 -0300

    06-HeroesApp 01

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class MaterialModule { }
