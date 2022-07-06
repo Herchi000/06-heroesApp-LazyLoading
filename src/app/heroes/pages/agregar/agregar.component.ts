5f82585a84c8de68e972b2d840474305365e9f4f
unknown <hugofer000@gmail.com>
2022-07-06 17:59:44 -0300

    06-HeroesApp 04

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap} from 'rxjs/operators'
+
 
 @Component({
   selector: 'app-agregar',
@@ -6,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  constructor() { }
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  }
+
+  constructor(private heroesService: HeroesService,
+              private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.heroesService.getHeroePorId(id))
+      )
+      .subscribe( heroe => this.heroe = heroe)
+
+  }
+
+  guardar(){
+    if(this.heroe.superhero.trim().length === 0){
+      return
+    }
+
+    this.heroesService.agregarHeroe(this.heroe)
+      .subscribe(res => {
+        console.log('Respuesta: ', res);
+      })
   }
 
 }
